43d7527074c8a072f77b9fde367d5c24f13d9e27
onozaty <onozaty@gmail.com>
2016-11-23 23:07:23 +0900

    クエリとEntityのマッピングにBeanPropertyRowMapperを使用するように変更。

--- spring-boot-rest-jdbc/src/main/java/com/example/domain/User.java
+++ spring-boot-rest-jdbc/src/main/java/com/example/domain/User.java
@@ -2,9 +2,11 @@ package com.example.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     private Integer id;
