a533705710b12e0a59c33d42c6c38f4da4f579cc
onozaty <onozaty@gmail.com>
2016-11-30 01:00:01 +0900

    ApiModelProperty を使用して、Modelの説明を変更

--- spring-boot-rest-jpa/src/main/java/com/example/domain/Customer.java
+++ spring-boot-rest-jpa/src/main/java/com/example/domain/Customer.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,9 +19,12 @@ public class Customer {
 
     @Id
     @GeneratedValue
+    @ApiModelProperty(position = 1, example = "1", value = "新規追加時は使用しません")
     private Integer id;
 
+    @ApiModelProperty(position = 2, example = "名前", required = true)
     private String name;
 
+    @ApiModelProperty(position = 3, example = "住所")
     private String address;
 }
