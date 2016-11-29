dc7ba88223855d70a1773553f374bf1134d074bf
onozaty <onozaty@gmail.com>
2016-11-30 00:19:43 +0900

    Swaggerの設定追加

--- spring-boot-rest-jpa/src/main/java/com/example/SpringBootRestJpaApplication.java
+++ spring-boot-rest-jpa/src/main/java/com/example/SpringBootRestJpaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootRestJpaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootRestJpaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestJpaApplication.class, args);
+    }
 }
