c385f8997dec79b39fdcdd5f330d5b99362faa16
onozaty <onozaty@gmail.com>
2017-01-05 00:34:32 +0900

    更新、削除処理追加。

--- spring-boot-mvc-simple/src/main/java/com/example/web/CustomerController.java
+++ spring-boot-mvc-simple/src/main/java/com/example/web/CustomerController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.domain.model.Customer;
 import com.example.domain.service.CustomerService;
@@ -48,7 +49,43 @@ public class CustomerController {
         BeanUtils.copyProperties(form, customer);
 
         customerService.create(customer);
-        
+
+        return "redirect:/customers";
+    }
+
+    @GetMapping(path = "edit", params = "form")
+    public String editForm(@RequestParam Integer id, CustomerForm form) {
+
+        Customer customer = customerService.findOne(id);
+        BeanUtils.copyProperties(customer, form);
+
+        return "customers/edit";
+    }
+
+    @PostMapping(path = "edit")
+    public String edit(@RequestParam Integer id, @Validated CustomerForm form, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return editForm(id, form);
+        }
+
+        Customer customer = new Customer();
+        BeanUtils.copyProperties(form, customer);
+        customer.setId(id);
+        customerService.update(customer);
+
+        return "redirect:/customers";
+    }
+
+    @RequestMapping(path = "edit", params = "goToTop")
+    public String goToTop() {
+        return "redirect:/customers";
+    }
+
+    @PostMapping(path = "delete")
+    public String delete(@RequestParam Integer id) {
+
+        customerService.delete(id);
         return "redirect:/customers";
     }
 }
