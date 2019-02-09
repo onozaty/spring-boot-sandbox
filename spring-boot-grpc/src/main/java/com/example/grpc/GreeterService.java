3077b16b4634475f93af55c87eb625be032ffc37
onozaty <onozaty@gmail.com>
2019-02-09 21:00:27 +0900

    [gRPC] 並列実行を確認

--- spring-boot-grpc/src/main/java/com/example/grpc/GreeterService.java
+++ spring-boot-grpc/src/main/java/com/example/grpc/GreeterService.java
@@ -6,17 +6,23 @@ import com.example.grpc.GreeterOuterClass.HelloReply;
 import com.example.grpc.GreeterOuterClass.HelloRequest;
 
 import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
 
 @GRpcService
+@Slf4j
 public class GreeterService extends GreeterGrpc.GreeterImplBase {
 
     @Override
     public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+
+        log.info("start: " + request.getName());
 
         HelloReply reply = HelloReply.newBuilder()
                 .setMessage("Hello " + request.getName())
                 .build();
         responseObserver.onNext(reply);
         responseObserver.onCompleted();
+
+        log.info("end: " + request.getName());
     }
 }
--- spring-boot-grpc/src/test/java/com/example/grpc/GreeterServiceTest.java
+++ spring-boot-grpc/src/test/java/com/example/grpc/GreeterServiceTest.java
@@ -2,6 +2,8 @@ package com.example.grpc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.stream.IntStream;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.lognet.springboot.grpc.context.LocalRunningGrpcPort;
@@ -39,5 +41,29 @@ public class GreeterServiceTest {
         HelloReply reply = stub.sayHello(request);
 
         assertThat(reply.getMessage()).isEqualTo("Hello " + name);
+    }
+
+    @Test
+    public void sayHello_並列実行() {
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", runningPort)
+                .usePlaintext()
+                .build();
+
+        GreeterGrpc.GreeterBlockingStub stub = GreeterGrpc.newBlockingStub(channel);
+
+        IntStream.range(0, 10).parallel().forEach(i -> {
+
+            String name = "Taro" + i;
+
+            HelloRequest request = HelloRequest.newBuilder()
+                    .setName(name)
+                    .build();
+
+            HelloReply reply = stub.sayHello(request);
+
+            assertThat(reply.getMessage()).isEqualTo("Hello " + name);
+
+        });
     }
 }
