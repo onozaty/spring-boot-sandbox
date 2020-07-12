8a27ddb59bc5f3b4a2a8ac499a045cbee84e5dbf
onozaty <onozaty@gmail.com>
2020-07-12 23:17:30 +0900

    [flyway-java-migration] FlywayのJava Migrationを使ったサンプル

---
+++ spring-boot-flyway-java-migration/src/main/java/com/github/onozaty/springboot/flyway/SpringBootFlywayJavaMigrationApplication.java
@@ -0,0 +1,13 @@
+package com.github.onozaty.springboot.flyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFlywayJavaMigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFlywayJavaMigrationApplication.class, args);
+	}
+
+}
