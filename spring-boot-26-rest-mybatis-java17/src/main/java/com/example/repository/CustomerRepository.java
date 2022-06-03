5a1218ae275c3773704d2a4204bae58799f34846
onozaty <onozaty@gmail.com>
2022-06-04 00:18:46 +0900

    [spring-boot-26-rest-mybatis-java17] SQLでTextBlockを使う形に変更

--- spring-boot-26-rest-mybatis-java17/src/main/java/com/example/repository/CustomerRepository.java
+++ spring-boot-26-rest-mybatis-java17/src/main/java/com/example/repository/CustomerRepository.java
@@ -17,25 +17,73 @@ import com.example.domain.Customer;
 @Mapper
 public interface CustomerRepository {
 
-    @Select("SELECT * FROM customers ORDER BY id")
+    @Select("""
+SELECT
+  *
+FROM
+  customers
+ORDER BY
+id
+    """)
     public List<Customer> findAll();
 
-    @Select("SELECT * FROM customers WHERE id = #{id}")
+    @Select("""
+SELECT
+  *
+FROM
+  customers
+WHERE
+  id = #{id}
+    """)
     public Customer findOne(@Param("id") Integer id);
 
-    @Insert("INSERT INTO customers(first_name, last_name, address) VALUES(#{firstName}, #{lastName}, #{address})")
+    @Insert("""
+INSERT INTO customers(
+  first_name
+  , last_name
+  , address
+)
+VALUES(
+  #{firstName}
+  , #{lastName}
+  , #{address}
+)
+""")
     @SelectKey(statement = "call identity()", keyProperty = "id", before = false, resultType = int.class)
     public void insert(Customer customer);
 
-    @Update("UPDATE customers SET first_name = #{firstName}, last_name = #{lastName}, address = #{address} WHERE id = #{id}")
+    @Update("""
+UPDATE customers
+  SET
+    first_name = #{firstName}
+    , last_name = #{lastName}
+    , address = #{address}
+WHERE
+  id = #{id}
+""")
     public void update(Customer customer);
 
-    @Delete("DELETE FROM customers WHERE id = #{id}")
+    @Delete("""
+DELETE FROM customers
+WHERE
+  id = #{id}
+    """)
     public void delete(@Param("id") Integer id);
 
-    @Select("SELECT * FROM customers WHERE first_name LIKE '%' || #{firstName} || '%' ORDER BY id")
+    @Select("""
+SELECT
+  *
+FROM
+  customers
+WHERE
+  first_name LIKE '%' || #{firstName} || '%'
+ORDER BY
+  id
+    """)
     public List<Customer> findByFirstName(@Param("firstName") String firstName);
 
-    @Delete("DELETE FROM customers")
+    @Delete("""
+DELETE FROM customers
+    """)
     public void deleteAll();
 }
