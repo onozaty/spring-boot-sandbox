85cf705f8a7602bb952700ffaf40b084d8b5d6a1
onozaty <onozaty@gmail.com>
2022-10-13 23:56:13 +0900

    [spring-boot-27-ldap] Springのガイドを元に設定
    https://spring.pleiades.io/guides/gs/authenticating-ldap/

---
+++ spring-boot-27-ldap/src/main/java/com/example/ldap/LdapApplication.java
@@ -0,0 +1,13 @@
+package com.example.ldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LdapApplication.class, args);
+    }
+
+}
---
+++ spring-boot-27-ldap/src/main/java/com/example/ldap/web/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.ldap.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Welcome to the home page!";
+    }
+}
