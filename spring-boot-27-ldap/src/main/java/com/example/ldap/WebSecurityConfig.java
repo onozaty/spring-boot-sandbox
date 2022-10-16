931d6b6cfd7ce329c7a7d5c9baa0c2c3eb8c91a3
onozaty <onozaty@gmail.com>
2022-10-16 15:02:59 +0900

    [spring-boot-27-ldap] Spring Security 5.7にあわせて変更＋権限毎に確認するためのHTML追加

--- spring-boot-27-ldap/src/main/java/com/example/ldap/WebSecurityConfig.java
+++ spring-boot-27-ldap/src/main/java/com/example/ldap/WebSecurityConfig.java
@@ -1,35 +1,42 @@
 package com.example.ldap;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.ldap.core.support.BaseLdapPathContextSource;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.config.ldap.LdapPasswordComparisonAuthenticationManagerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http
                 .authorizeRequests()
                 .anyRequest().fullyAuthenticated()
                 .and()
                 .formLogin();
+
+        return http.build();
     }
 
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .ldapAuthentication()
-                .userDnPatterns("uid={0},ou=people")
-                .groupSearchBase("ou=groups")
-                .contextSource()
-                .url("ldap://localhost:8389/dc=example,dc=com")
-                .and()
-                .passwordCompare()
-                .passwordEncoder(NoOpPasswordEncoder.getInstance())
-                .passwordAttribute("userPassword");
+    @Bean
+    public AuthenticationManager authenticationManager(BaseLdapPathContextSource contextSource) {
+        LdapPasswordComparisonAuthenticationManagerFactory factory =
+                new LdapPasswordComparisonAuthenticationManagerFactory(
+                        contextSource, new BCryptPasswordEncoder());
+        factory.setUserDnPatterns("uid={0},ou=people");
+        factory.setPasswordAttribute("userPassword");
+        return factory.createAuthenticationManager();
     }
 
+    @Bean
+    public BaseLdapPathContextSource contextSource() {
+        return new DefaultSpringSecurityContextSource("ldap://localhost:8389/dc=example,dc=com");
+    }
 }
