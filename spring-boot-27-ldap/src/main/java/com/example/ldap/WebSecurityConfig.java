9a8c434f3b80a4bb363fa9bbadc0f2f136cd067c
onozaty <onozaty@gmail.com>
2022-10-19 00:06:06 +0900

    [spring-boot-27-ldap] 権限によるチェック、ログアウト追加

--- spring-boot-27-ldap/src/main/java/com/example/ldap/WebSecurityConfig.java
+++ spring-boot-27-ldap/src/main/java/com/example/ldap/WebSecurityConfig.java
@@ -8,7 +8,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.ldap.LdapPasswordComparisonAuthenticationManagerFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+import org.springframework.security.ldap.userdetails.DefaultLdapAuthoritiesPopulator;
+import org.springframework.security.ldap.userdetails.LdapAuthoritiesPopulator;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class WebSecurityConfig {
@@ -16,27 +19,59 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http
-                .authorizeRequests()
-                .anyRequest().fullyAuthenticated()
-                .and()
-                .formLogin();
+        // https://docs.spring.io/spring-security/reference/servlet/authentication/passwords/form.html
+        // デフォルトのフォーム認証を利用
+        http.formLogin();
+        http.logout(
+                // GETでもログアウトが受け付けられるように
+                logout -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout")));
+
+        // https://docs.spring.io/spring-security/reference/servlet/authorization/authorize-http-requests.html
+        http.authorizeHttpRequests(
+                authz -> authz
+                        // admin配下は要ADMINロール
+                        .mvcMatchers("/admin/**").hasRole("ADMIN")
+                        // user配下は要USERロール
+                        .mvcMatchers("/user/**").hasRole("USER")
+                        // それ以外のリクエストも全て認証必要(ロールは何でもOK)
+                        .anyRequest().authenticated());
 
         return http.build();
     }
 
-    @Bean
-    public AuthenticationManager authenticationManager(BaseLdapPathContextSource contextSource) {
-        LdapPasswordComparisonAuthenticationManagerFactory factory =
-                new LdapPasswordComparisonAuthenticationManagerFactory(
-                        contextSource, new BCryptPasswordEncoder());
-        factory.setUserDnPatterns("uid={0},ou=people");
-        factory.setPasswordAttribute("userPassword");
-        return factory.createAuthenticationManager();
-    }
-
+    // https://docs.spring.io/spring-security/reference/servlet/authentication/passwords/ldap.html
     @Bean
     public BaseLdapPathContextSource contextSource() {
         return new DefaultSpringSecurityContextSource("ldap://localhost:8389/dc=example,dc=com");
+    }
+
+    @Bean
+    public LdapAuthoritiesPopulator authorities(BaseLdapPathContextSource contextSource) {
+
+        // 権限の情報をgroupsから取得
+        String groupSearchBase = "ou=groups";
+
+        DefaultLdapAuthoritiesPopulator authorities =
+                new DefaultLdapAuthoritiesPopulator(contextSource, groupSearchBase);
+        // uniqueMemberが一致するものが対象
+        authorities.setGroupSearchFilter("uniqueMember={0}");
+
+        return authorities;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(BaseLdapPathContextSource contextSource,
+            LdapAuthoritiesPopulator authorities) {
+
+        LdapPasswordComparisonAuthenticationManagerFactory factory =
+                new LdapPasswordComparisonAuthenticationManagerFactory(contextSource, new BCryptPasswordEncoder());
+
+        // ユーザ名とパスワードのフィールドを指定
+        factory.setUserDnPatterns("uid={0},ou=people");
+        factory.setPasswordAttribute("userPassword");
+
+        factory.setLdapAuthoritiesPopulator(authorities);
+
+        return factory.createAuthenticationManager();
     }
 }
