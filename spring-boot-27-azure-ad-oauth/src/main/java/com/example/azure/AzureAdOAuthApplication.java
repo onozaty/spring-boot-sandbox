c5bc9aef6e2a4d6fdea710fc1f667eb9e3dd02d4
onozaty <onozaty@gmail.com>
2022-10-23 17:33:05 +0900

    [spring-boot-27-azure-ad-oauth] とりあえず認証通るところまで

---
+++ spring-boot-27-azure-ad-oauth/src/main/java/com/example/azure/AzureAdOAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureAdOAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureAdOAuthApplication.class, args);
+	}
+
+}
